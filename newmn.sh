42d99c49294b2383822f6b550824554e83c16ffb
igxer <37401129+igxer@users.noreply.github.com>
2018-08-07 08:25:52 +0700

    Update newmn.sh

--- newmn.sh
+++ newmn.sh
@@ -1,1 +1,12 @@
 
+apt update
+apt -y install software-properties-common
+apt-add-repository -y ppa:bitcoin/bitcoin
+apt update
+apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
+libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git curl libdb4.8-dev \
+bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw fail2ban pkg-config libevent-dev libzmq5
+apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
+libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git curl libdb4.8-dev \
+bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw fail2ban pkg-config libevent-dev libzmq5
+apt -y upgrade
