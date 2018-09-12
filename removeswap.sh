f1fc902fbeaa1aaa48d18971472ee8556094fe30
igxer <37401129+igxer@users.noreply.github.com>
2018-09-12 21:00:08 +0700

    Create removeswap.sh

---
+++ removeswap.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+sudo +x remove_swap.sh
+# does the swap file exist?
+grep -q "swapfile" /etc/fstab
+
+# if it does then remove it
+if [ $? -eq 0 ]; then
+	echo 'swapfile found. Removing swapfile.'
+	sed -i '/swapfile/d' /etc/fstab
+	echo "3" > /proc/sys/vm/drop_caches
+	swapoff -a
+	rm -f /swapfile
+else
+	echo 'No swapfile found. No changes made.'
+fi
+
+echo '--------------------------------------------'
+echo 'Check whether the swap space removed or not?'
+echo '--------------------------------------------'
+swapon --show
